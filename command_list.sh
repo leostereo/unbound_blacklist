2a6d5c3ca23face680e3eae1a4869a72e089c68c
Your Name <you@example.com>
2021-01-11 13:39:59 +0000

    11_01_2021

--- command_list.sh
+++ command_list.sh
@@ -1,10 +1,11 @@
 #curl -sSf "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" always_nxdomain"}' > /etc/unbound/blacklists/stevenblack.conf
+#curl -sSf "https://mirror1.malwaredomains.com/files/justdomains" | awk '{print "local-zone: \""$1"\" always_nxdomain"}' | grep -v 'local-zone: "" always_nxdomain'  > /etc/unbound/blacklists/malwaredomains.conf
 #energized
 #curl -sSf "https://block.energized.pro/ultimate/formats/domains.txt" | grep -v "^#"  | awk '{print "local-zone: \""$1"\" always_nxdomain"}' > /etc/unbound/blacklists/energized.conf
 
 
 #Activas
-curl -sSf "https://mirror1.malwaredomains.com/files/justdomains" | awk '{print "local-zone: \""$1"\" always_nxdomain"}' | grep -v 'local-zone: "" always_nxdomain'  > /etc/unbound/blacklists/malwaredomains.conf
+curl -sSf "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" always_nxdomain"}' > /etc/unbound/blacklists/stevenblack.conf
 
 
 result=$(/usr/sbin/unbound-checkconf);
